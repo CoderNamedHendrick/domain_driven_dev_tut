67825fc73c011f911207bc8a1425e01b3e06c528
Hendrick <sebastinesoacatp@gmail.com>
2023-01-30 17:50:43 +0100

    feature: note application layer

--- lib/injection.config.dart
+++ lib/injection.config.dart
@@ -6,19 +6,21 @@
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:cloud_firestore/cloud_firestore.dart' as _i9;
-import 'package:domain_driven_tut/application/auth/auth_bloc.dart' as _i13;
+import 'package:domain_driven_tut/application/auth/auth_bloc.dart' as _i14;
 import 'package:domain_driven_tut/application/auth/sign_in_form/sign_in_form_bloc.dart'
-    as _i12;
+    as _i13;
 import 'package:domain_driven_tut/application/notes/note_actor/note_actor_bloc.dart'
     as _i10;
-import 'package:domain_driven_tut/application/notes/note_watcher/note_watcher_bloc.dart'
+import 'package:domain_driven_tut/application/notes/note_form/note_form_bloc.dart'
     as _i11;
+import 'package:domain_driven_tut/application/notes/note_watcher/note_watcher_bloc.dart'
+    as _i12;
 import 'package:domain_driven_tut/domain/auth/i_auth_facade.dart' as _i5;
 import 'package:domain_driven_tut/domain/notes/i_note_repository.dart' as _i7;
 import 'package:domain_driven_tut/infrastructure/auth/firebase_auth_facade.dart'
     as _i6;
 import 'package:domain_driven_tut/infrastructure/core/firebase_injectable_module.dart'
-    as _i14;
+    as _i15;
 import 'package:domain_driven_tut/infrastructure/notes/note_repository.dart'
     as _i8;
 import 'package:firebase_auth/firebase_auth.dart' as _i3;
@@ -52,13 +54,15 @@ extension GetItInjectableX on _i1.GetIt {
         () => _i8.NoteRepository(gh<_i9.FirebaseFirestore>()));
     gh.factory<_i10.NoteActorBloc>(
         () => _i10.NoteActorBloc(gh<_i7.INoteRepository>()));
-    gh.factory<_i11.NoteWatcherBloc>(
-        () => _i11.NoteWatcherBloc(gh<_i7.INoteRepository>()));
-    gh.factory<_i12.SignInFormBloc>(
-        () => _i12.SignInFormBloc(gh<_i5.IAuthFacade>()));
-    gh.factory<_i13.AuthBloc>(() => _i13.AuthBloc(gh<_i5.IAuthFacade>()));
+    gh.factory<_i11.NoteFormBloc>(
+        () => _i11.NoteFormBloc(gh<_i7.INoteRepository>()));
+    gh.factory<_i12.NoteWatcherBloc>(
+        () => _i12.NoteWatcherBloc(gh<_i7.INoteRepository>()));
+    gh.factory<_i13.SignInFormBloc>(
+        () => _i13.SignInFormBloc(gh<_i5.IAuthFacade>()));
+    gh.factory<_i14.AuthBloc>(() => _i14.AuthBloc(gh<_i5.IAuthFacade>()));
     return this;
   }
 }
 
-class _$FirebaseInjectableModule extends _i14.FirebaseInjectableModule {}
+class _$FirebaseInjectableModule extends _i15.FirebaseInjectableModule {}
